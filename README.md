c3a31de7d173503970eefc7dffb76c5cc5b4034c
san-ya <snirya1@gmail.com>
2020-11-03 17:32:38 +0200

    First

---
+++ README.md
@@ -0,0 +1,26 @@
+# Snir Yadid Personal website
+
+This is my personal website built using ReactJS and Bulma as the CSS framework. [Live here](https://snir-yadid-portfolio.netlify.app/)
+
+The personal information on the website is populated from a json file that follows the [JSON Resume](https://jsonresume.org/) open source standard.
+
+## Build
+
+1. Clone the repo:
+
+```console
+> git clone https://github.com/jcoelho93/personal-website.git
+```
+
+2. Install dependencies and run build command:
+
+```console
+> npm install
+> npm run build
+```
+
+You can also test the app with a development server, just run:
+
+```console
+> npm start
+```
---
+++ src/components/structure/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer has-background-link">
+      <div className="content has-text-centered has-text-white">
+        <p>
+          Built at <i className="fas fa-moon"></i> by{" "}
+          <a href="https://github.com/san-ya" className="has-text-white">
+            <strong>Snir Yadid</strong>
+          </a>
+        </p>
+        <p>
+          <img
+            width="512"
+            height="96"
+            src="https://bulma.io/images/made-with-bulma--semiwhite.png"
+            className="bulma-image"
+            alt=""
+          />
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
